8efbf06dcb7fdfde3dec8d29c51eb32650be0e6b
Samuel Escobar <sescobar8714@gmail.com>
2020-02-17 05:46:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,28 +3,37 @@ require 'yaml'
 require 'pry'
 
 def load_library(file_path)
-  file = File.read(file_path)
-  emoticons = YAML.load(file)
-  new_hash = {
-   "get_meaning" => {},
-   "get_emoticon" => {}
- }
-emoticons.each do |name,emojis|
-  new_hash["get_meaning"][emojis[1]] = name
-  new_hash["get_emoticon"][emojis[0]] = emojis[1]
+  emoticons {"get_meaning"=>{},
+"get_emoticon"=>{}}
+YAML.load_file(file_path).each do |meaning, describe|
+  eng,jan = describe
+  emoticons["get_meaning"][jan] = meaning
+  emoticons["get_emoticon"][eng] = jan
 end
-return new_hash
+emoticons
+end
+#  file = File.read(file_path)
+#  emoticons = YAML.load(file)
+#  new_hash = {
+#   "get_meaning" => {},
+#   "get_emoticon" => {}
+# }
+#emoticons.each do |name,emojis|
+  #new_hash["get_meaning"][emojis[1]] = name
+#  new_hash["get_emoticon"][emojis[0]] = emojis[1]
+end
+#return new_hash
 end
 
 
 
 def get_japanese_emoticon(file_path, emoticon)
   #binding.pry
-  library = load_library(file_path)
-  library["get_emoticon"].each do |name, emojis|
-
-
+  #library = load_library(file_path)
+  #library["get_emoticon"].each do |name, emojis|
+    #result = 
   end
+  
 end
 
 def get_english_meaning(file_path, emoticon)
