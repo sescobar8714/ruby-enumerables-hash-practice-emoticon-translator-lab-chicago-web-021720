da237bdd437d9b72bbccc0bbe7721d437c8fcdf3
Samuel Escobar <sescobar8714@gmail.com>
2020-02-17 05:50:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,10 +32,24 @@ def get_japanese_emoticon(file_path, emoticon)
   #library = load_library(file_path)
   #library["get_emoticon"].each do |name, emojis|
     #result =
+    def get_japanese_emoticon(file_path, emoticon)
+      emoticons = load_library(file_path)
+      result = emoticons["get_emoticon"][emoticon]
+      if result
+        result
+      else
+        "Sorry, that emoticon was not found"
+      end
   end
 
-end
 
 def get_english_meaning(file_path, emoticon)
 
+emoticons = load_library(file_path)
+result = emoticons["get_meaning"][emoticon]
+if result
+  result
+else
+  "Sorry, That emoticon was not found"
+end
 end
